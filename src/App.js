49102d04d38201ebc88fb61f85e5c238b384dda7
RomanczuG <romanczug@icloud.com>
2023-04-24 14:33:07 -0400

    new

--- src/App.js
+++ src/App.js
@@ -61,93 +61,113 @@ function App() {
       {/* Features Section */}
 
       <section
-        id="features"
-        className="container mx-auto px-4 py-20 text-white"
-      >
-        <h2 className="text-4xl font-semibold text-center mb-12">Features</h2>
-        <div className="grid grid-cols-1 gap-8">
-          {/* Feature 1 */}
-          <div className="flex flex-col-reverse md:flex-row items-center">
-            <div className="md:w-1/2 mb-6 md:mb-0">
-              <img
-                src={feature_1}
-                alt="Get Started"
-                className="w-full h-auto rounded-lg"
-              />
-            </div>
-            <div className="md:w-1/2 md:ml-8">
-              <h3 className="text-2xl font-semibold">Get Started</h3>
-              <p className="mt-4">
-                Drag and drop or select your lecture or presentation slides in
-                PDF format!
-              </p>
-            </div>
-          </div>
-          {/* Feature 2 */}
-          <div className="flex flex-col md:flex-row items-center">
-            <div className="md:w-1/2 md:order-2">
-              <img
-                src={feature_2}
-                alt="Download Your Notes"
-                className="w-full h-auto mb-6 md:mb-0 rounded-lg"
-              />
-            </div>
-            <div className="md:w-1/2 md:mr-8">
-              <h3 className="text-2xl font-semibold">Download Your Notes</h3>
-              <p className="mt-4">
-                Our powerful AI creates comprehensive notes, are ready for you
-                to download and study effortlessly.
-              </p>
-            </div>
-          </div>
-        </div>
-        {/* Demo Link */}
-        <div className="text-center mt-12">
-          <a
-            href="#demo"
-            className="text-white text-2xl font-semibold hover:text-gray-300"
-          >
-            Check out our demo
-          </a>
-        </div>
-      </section>
+  id="features"
+  className="container mx-auto px-4 py-20 text-white"
+>
+  <h2 className="text-4xl font-semibold text-center mb-12">Features</h2>
+  <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+    {/* Feature 1 */}
+    <div className="flex flex-col-reverse md:flex-row items-center md:space-x-8">
+      <div className="md:w-1/2 mb-6 md:mb-0">
+        <img
+          src={feature_1}
+          alt="Get Started"
+          className="w-full h-auto rounded-lg"
+        />
+      </div>
+      <div className="md:w-1/2">
+        <h3 className="text-2xl font-semibold">Get Started</h3>
+        <p className="mt-4">
+          Drag and drop or select your HW in
+          PDF format! StudyBoost's intuitive interface makes it simple to
+          upload your files and start the AI-driven note generation process.
+        </p>
+      </div>
+    </div>
+    {/* Feature 2 */}
+    <div className="flex flex-col md:flex-row items-center md:space-x-8">
+      <div className="md:w-1/2 md:mb-2">
+        <img
+          src={feature_2}
+          alt="Download Your Notes"
+          className="w-full h-auto mb-6 md:mb-0 rounded-lg"
+        />
+      </div>
+      <div className="md:w-1/2">
+        <h3 className="text-2xl font-semibold">Download Your Notes</h3>
+        <p className="mt-4">
+          Our powerful AI creates comprehensive notes, are ready for you
+          to download and study effortlessly. Say goodbye to manually
+          searching for terms, formulas, descriptions etc. and focus on learning and solving the problems more efficiently.
+        </p>
+      </div>
+    </div>
+  </div>
+  {/* Demo Button */}
+  <div className="text-center mt-12 ">
+  <button className="text-black mt-4 w-full md:w-auto px-6 py-2 bg-white text-252D62 font-semibold rounded-lg">
+      Check out my demo
+    </button>
+  </div>
+</section>
+
 
       {/* About the Author Section */}
-      <section
-        id="about-author"
-        className="container mx-auto px-4 py-20 text-white"
-      >
-        <h2 className="text-4xl font-semibold text-center mb-12">
-          About the Author
-        </h2>
-        <div className="flex flex-col items-center md:flex-row">
-          {/* Author Image */}
-          <div className="md:w-1/2 mb-8 md:mb-0">
-            <img
-              src="author-image.jpg"
-              alt="Author"
-              className="w-full h-auto rounded-full"
-            />
-          </div>
-          {/* Author Bio */}
-          <div className="md:w-1/2 md:ml-8">
-            <h3 className="text-2xl font-semibold mb-4">John Doe</h3>
-            <p>
-              John Doe is an experienced educator and software engineer with a
-              passion for harnessing the power of technology to improve
-              education. With a background in computer science and years of
-              teaching experience, John has developed StudyBoost to help
-              students learn more effectively and independently.
-            </p>
-            <p className="mt-4">
-              His dedication to creating innovative learning solutions has been
-              recognized by leading institutions and publications. John's
-              mission is to empower students worldwide, providing them with the
-              tools they need to excel in their academic journey.
-            </p>
-          </div>
-        </div>
-      </section>
+      {/* About the Author Section */}
+<section
+  id="about-author"
+  className="container mx-auto px-4 py-20 text-white"
+>
+  <h2 className="text-4xl font-semibold text-center mb-12">
+    About the Author
+  </h2>
+  <div className="flex flex-col items-center">
+    {/* Author Bio */}
+    <div className="w-full md:w-2/3 text-center">
+      <h3 className="text-2xl font-semibold mb-4">Mateusz Romaniuk</h3>
+      <p>
+        I am a student, and this is one of my side projects. If you love
+        coding like me, please consider using another free tool that I have
+        made called{" "}
+        <a
+          href="https://codescribe.app"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-blue-300 hover:text-blue-500"
+        >
+          codescribe.app
+        </a>
+        . It helps you document, explain, optimize, and debug code thanks to
+        AI.
+      </p>
+      <p className="mt-4">
+        If you like my work, consider following me on Twitter or buying me a
+        coffee.
+      </p>
+      <div className="mt-8 flex flex-col space-y-4 md:space-y-0 md:flex-row md:justify-center md:space-x-4">
+        <a
+          href="https://twitter.com/your_username"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-black px-6 py-2 bg-white text-252D62 font-semibold rounded-lg"
+        >
+          Follow on Twitter
+          
+        </a>
+        <a
+          href="https://www.buymeacoffee.com/mateuszcoder"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-black px-6 py-2 bg-white text-252D62 font-semibold rounded-lg"
+        >
+          Buy me a coffee
+        </a>
+      </div>
+      
+    </div>
+  </div>
+</section>
+
 
       {/* Add more sections as needed */}
     </div>
