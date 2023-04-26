9ddeb103f264ac0f914513652e690ac3b5b1caa3
RomanczuG <romanczug@icloud.com>
2023-04-26 14:10:41 -0400

    new

--- src/pages/Tool.js
+++ src/pages/Tool.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { Disclosure } from "@headlessui/react";
-import CryptoJS from 'crypto-js';
+import CryptoJS from "crypto-js";
 
 const Tool = () => {
   const sampleData = [
@@ -12,12 +12,14 @@ const Tool = () => {
     // Add more questions and analyses as needed
   ];
   const client = axios.create({
-    baseURL: "http://127.0.0.1:5000",
+    // baseURL: "http://127.0.0.1:5000",
+    baseURL: "https://studyboost.uc.r.appspot.com"
     // timeout: 5000, // Add a 5 seconds timeout
   });
   const [file, setFile] = useState(null);
   const [output, setOutput] = useState(sampleData);
   const [pdfSrc, setPdfSrc] = useState(null);
+  const [uploaded, setUploaded] = useState(false);
 
   // ! FILE UPLOAD
   const handleFileChange = (e) => {
@@ -28,7 +30,7 @@ const Tool = () => {
     const timestamp = new Date().getTime();
     const uniqueFilename = `${timestamp}-${filename}`;
     const hashedFilename = CryptoJS.SHA256(uniqueFilename).toString();
-    const fileExtension = filename.split('.').pop();
+    const fileExtension = filename.split(".").pop();
     return `${hashedFilename}.${fileExtension}`;
   };
 
@@ -56,6 +58,7 @@ const Tool = () => {
         alert("File uploaded successfully");
         console.log("File uploaded successfully");
         setUploadedFilename(response.data.filename); // Store the filename in the state
+        setUploaded(true);
       } else {
         alert("File upload failed");
         console.error("File upload failed");
@@ -74,9 +77,9 @@ const Tool = () => {
         alert("Please upload a file first");
         return;
       }
-      console.log('Generating help...');
+      console.log("Generating help...");
       const response = await client.get(`/api/generate/${uploadedFilename}`);
-      console.log('Generated help:', response.data);
+      console.log("Generated help:", response.data);
       // const formattedData = response.data.map((question, help) => {
       //   return {
       //     question: question,
@@ -117,7 +120,21 @@ const Tool = () => {
           help.
         </p>
         <div className="flex flex-col space-y-4 items-center">
-          <label className="bg-white text-black py-2 px-4 border border-gray-200 rounded-md cursor-pointer hover:bg-gray-100 transition duration-300">
+          <label className="bg-white flex justify-center text-black py-2 px-4 border border-gray-200 rounded-md cursor-pointer hover:bg-gray-100 transition duration-300">
+            {pdfSrc && (
+              <svg
+                className="w-6 h-6 text-black"
+                fill="none"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path d="M5 13l4 4L19 7" />
+              </svg>
+            )}
+
             {file ? file.name : "Choose File"}
             <input
               type="file"
@@ -126,33 +143,58 @@ const Tool = () => {
               className="hidden"
             />
           </label>
-          <button
-            onClick={handleUpload}
-            className="bg-indigo-600 hover:bg-indigo-700 w-40 text-white rounded-md px-3 py-2 transition duration-300"
-          >
-            Upload PDF File
-          </button>
-          <div className="mt-8">
-            {}
+          {pdfSrc && (
             <button
-              onClick={handleGenerate}
-              className="bg-indigo-600 hover:bg-indigo-700 w-40 text-white rounded-md px-3 py-2 transition duration-300"
+              onClick={handleUpload}
+              className="bg-indigo-600 flex hover:bg-indigo-700 w-48 justify-center text-white rounded-md px-3 py-2 transition duration-300"
             >
-              Generate Help
+              {uploaded && (
+                <svg
+                  className="w-6 h-6 text-white"
+                  fill="none"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path d="M5 13l4 4L19 7" />
+                </svg>
+              )}
+              Upload PDF File
             </button>
+          )}
+
+          <div className="mt-8">
+            {uploaded && (
+              <button
+                onClick={handleGenerate}
+                className="flex bg-indigo-600 hover:bg-indigo-700 w-48 justify-center text-white rounded-md px-3 py-2 transition duration-300"
+              >
+                {/* <svg
+                  className="w-6 h-6 text-white"
+                  fill="none"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path d="M5 13l4 4L19 7" />
+                </svg> */}
+                Generate Help
+              </button>
+            )}
           </div>
         </div>
 
         <div className="mt-8">
-
-        
           {output !== sampleData && (
             <h2 className="text-2xl font-semibold text-white mb-4">
-            Questions & Analysis
-          </h2>
-          )
-          }
-          
+              Questions & Analysis
+            </h2>
+          )}
+
           <div className="w-full space-y-4">
             {output.map((item, index) => (
               <Disclosure key={index}>
@@ -160,19 +202,19 @@ const Tool = () => {
                   <>
                     <Disclosure.Button className="flex justify-between w-full px-4 py-2 text-sm font-semibold text-left text-white bg-indigo-600 hover:bg-indigo-700 rounded-lg focus:outline-none">
                       <p>
-                      {item.question.split('\n').map((line, index) => {
-                        return <div key={index}>{line}</div>
-                      })}
-                      {/* <span>{item.question}</span> */}
+                        {item.question.split("\n").map((line, index) => {
+                          return <div key={index}>{line}</div>;
+                        })}
+                        {/* <span>{item.question}</span> */}
                       </p>
                       <span>{open ? "-" : "+"}</span>
                     </Disclosure.Button>
                     <Disclosure.Panel className="px-4 pt-4 pb-2 text-white bg-gray-800 rounded-b-lg">
                       <p>
-                      {/* {item.analysis} */}
-                      {item.analysis.split('\n').map((line, index) => {
-                        return <div key={index}>{line}</div>
-                      })}
+                        {/* {item.analysis} */}
+                        {item.analysis.split("\n").map((line, index) => {
+                          return <div key={index}>{line}</div>;
+                        })}
                       </p>
                     </Disclosure.Panel>
                   </>
