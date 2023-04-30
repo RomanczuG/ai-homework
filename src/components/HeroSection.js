8d76d92af2a9e1c963039f21ee084c5e47dee89e
RomanczuG <romanczug@icloud.com>
2023-04-30 12:37:04 -0400

    new hero

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,12 +1,22 @@
-import React from "react";
-// import logo from "../hero.svg";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import { ClipLoader } from "react-spinners";
+
 export const HeroSection = ({
   waitlistData,
   error,
   loading,
   submitWaitlist,
 }) => {
+  const [showModal, setShowModal] = useState(false);
+
+  const openModal = () => {
+    setShowModal(true);
+  };
+
+  const closeModal = () => {
+    setShowModal(false);
+  };
   return (
     <header className="container mx-auto px-4 py-20 flex flex-col items-center md:items-start md:flex-row ">
       <div className="md:w-1/2 self-center">
@@ -19,48 +29,90 @@ export const HeroSection = ({
         </p>
         <div className="mt-4 w-full md:w-auto">
           {!waitlistData ? (
-            <form className="w-full">
-              <div className="flex flex-col space-y-4">
-                <div className="flex flex-col space-y-2">
-                  <input
-                    id="email"
-                    name="email"
-                    type="email"
-                    placeholder="Please enter your email"
-                    autoComplete="email"
-                    onChange={(e) => e.stopPropagation()}
-                    required
-                    className="rounded-md border border-gray-200 text-base p-2 text-gray-700"
-                  />
-                </div>
-              </div>
-              <div className="mt-4">
-                <button
-                  type="button"
-                  disabled={loading}
-                  onClick={() => {
-                    submitWaitlist({
-                      email: document.getElementById("email").value,
-                    });
-                  }}
-                  className="bg-indigo-600 hover:bg-indigo-700 text-white rounded-md px-3 py-2 w-full transition duration-300"
-                >
-                  {loading ? (
-                    <ClipLoader size={25} color={"#ffffff"} loading={true} />
-                  ) : (
-                    "Join the Waitlist"
-                  )}
-                </button>
-                {error && (
-                  <div className="text-center mt-2 text-xs text-red-500 px-6">
-                    {error}
+            <>
+            <button
+              onClick={openModal}
+              className="bg-indigo-600 hover:bg-indigo-700 text-white rounded-md px-3 py-2 w-full transition duration-300 "
+            >
+              Sign Up for Newsletter
+            </button>
+            {showModal && (<>
+              <div
+                      onClick={closeModal}
+                      className="fixed z-0 inset-0 bg-black opacity-50"
+                    ></div>
+              <div className="fixed z-10 inset-0 overflow-y-auto">
+                <div className="flex items-center justify-center min-h-screen">
+                  <div className="relative bg-white p-8 rounded shadow-md w-full max-w-md">
+                  <button
+                      onClick={closeModal}
+                      className="mr-2 absolute top-1 right-1 text-gray-400 hover:text-gray-600"
+                    >
+                      &times;
+                    </button>
+                    <h2 className="text-xl font-semibold mb-4">
+                      Sign up for our newsletter
+                    </h2>
+                    <form className="w-full">
+                      <div className="flex flex-col space-y-4">
+                        <div className="flex flex-col space-y-2">
+                          <input
+                            id="email"
+                            name="email"
+                            type="email"
+                            placeholder="Please enter your email"
+                            autoComplete="email"
+                            onChange={(e) => e.stopPropagation()}
+                            required
+                            className="rounded-md border border-gray-200 text-base p-2 text-gray-700"
+                          />
+                        </div>
+                      </div>
+                      <div className="mt-4">
+                        <button
+                          type="button"
+                          disabled={loading}
+                          onClick={() => {
+                            submitWaitlist({
+                              email: document.getElementById("email").value,
+                            });
+                            closeModal();
+                          }}
+                          className="bg-indigo-600 hover:bg-indigo-700 text-white rounded-md px-3 py-2 w-full transition duration-300"
+                        >
+                          {loading ? (
+                            <ClipLoader
+                              size={25}
+                              color={"#ffffff"}
+                              loading={true}
+                            />
+                          ) : (
+                            "Join the Waitlist"
+                            )}
+                            </button>
+                            {error && (
+                              <div className="text-center mt-2 text-xs text-red-500 px-6">
+                                {error}
+                              </div>
+                            )}
+                          </div>
+                        </form>
+                      </div>
+                    </div>
+                    
                   </div>
+                  </>
                 )}
-              </div>
-            </form>
-          ) : (
-            <div className="text-white">Thank you for signing up.</div>
-          )}
+              </>) : (
+        <div className="text-white">Thank you for signing up.</div>
+      )}
+      <button
+  className="bg-indigo-600 hover:bg-indigo-700 text-white rounded-md px-3 py-2 w-full transition duration-300 mt-4"
+>
+  <Link to="/tool">
+    Start Using the Tool
+  </Link>
+</button>
         </div>
       </div>
       <div className="mt-8 md:mt-0 md:w-1/2">
