cde6c1f765afadfc2bfa68cd6c7db37c78c31481
RomanczuG <romanczug@icloud.com>
2023-05-02 19:36:05 -0400

    new

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -19,7 +19,7 @@ export const LoginPage = () => {
     setLoading(true);
     setError(null);
 
-    const { user, error } = await supabase.auth.signIn({ email, password });
+    const { user, error } = await supabase.auth.signInWithPassword({ email, password });
 
     if (error) {
       setError(error.message);
