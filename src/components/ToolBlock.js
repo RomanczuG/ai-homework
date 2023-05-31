b1afb5085d974b8aea0320ddfc501170af53e6aa
RomanczuG <romanczug@icloud.com>
2023-05-31 18:19:58 +0200

    beautiful

--- src/components/ToolBlock.js
+++ src/components/ToolBlock.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState} from "react";
 import axios from "axios";
 import { ClipLoader } from "react-spinners";
 
@@ -17,9 +17,10 @@ const ToolBlock = () => {
     // baseURL: "http://127.0.0.1:5000",
     baseURL: "https://studyboost.uc.r.appspot.com",
   });
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading] = useState(false);
   const [stage, setStage] = useState(0);
   const [file, setFile] = useState(null);
+  const sampleData = [];
   const [output, setOutput] = useState(sampleData);
   const [outputLoading, setOutputLoading] = useState(false);
   const [pdfSrc, setPdfSrc] = useState(null);
@@ -27,14 +28,13 @@ const ToolBlock = () => {
   const [uploadedLoading, setUploadedLoading] = useState(false);
   const [generated, setGenerated] = useState(false);
   const [uploadedFilename, setUploadedFilename] = useState(null);
-  const sampleData = [];
+  // Some random dict in sampleData
+
 
   return (
     <section className="container mx-auto px-4 py-4">
       <div className="bg-[#F0FFE0] flex flex-col space-y-4 items-center">
         <label className="bg-white flex justify-center text-black py-2 px-4 border border-gray-200 rounded-md cursor-pointer hover:bg-gray-100 transition duration-300">
-          {pdfSrc && <CheckIcon />}
-
           {file ? file.name : "Choose File"}
           <input
             type="file"
@@ -47,17 +47,17 @@ const ToolBlock = () => {
         </label>
         {pdfSrc && (
           <Button
-          onClick={() =>
-            handleUpload(
-              client,
-              file,
-              setUploadedLoading,
-              setUploadedFilename,
-              setUploaded
-            )
-          }
-        >
-          {uploaded && <CheckIcon />}
+            onClick={() =>
+              handleUpload(
+                client,
+                file,
+                setUploadedLoading,
+                setUploadedFilename,
+                setUploaded
+              )
+            }
+          >
+            {uploaded && <CheckIcon />}
             {uploadedLoading && (
               <ClipLoader
                 className="mr-1"
@@ -67,68 +67,65 @@ const ToolBlock = () => {
               />
             )}
             Upload PDF File
-        </Button>
+          </Button>
         )}
 
-        {/* <div className="mt-8">
-          <div className="flex flex-col items-center"> */}
-            {uploaded && !outputLoading && (
-              <Button 
-                onClick={() =>
-                  handleStages(
-                    setOutputLoading,
-                    uploadedFilename,
-                    client,
-                    setOutput,
-                    output,
-                    setGenerated,
-                    setStage
-                  )
-                }
-              >
-                {generated && <CheckIcon />}
-                Generate Help
-  
-              </Button>
+        {uploaded && !outputLoading && (
+          <Button
+            onClick={() =>
+              handleStages(
+                setOutputLoading,
+                uploadedFilename,
+                client,
+                setOutput,
+                output,
+                setGenerated,
+                setStage
+              )
+            }
+          >
+            {generated && <CheckIcon />}
+            Generate Help
+          </Button>
+        )}
+        <div className="mt-8">
+          <div className="w-full h-2 flex flex-col items-center">
+            {outputLoading && (
+              <>
+                <p className="text-xl text-black mb-4">
+                  It can take up to 20/30 seconds per question to generate
+                  an analysis. Get a coffe or tea and relax &#x2615;
+                </p>
+                <div className="flex space-x-4 mb-4">
+                  <Stage
+                    stageNumber={1}
+                    currentStage={stage}
+                    stageName="Scanning Text"
+                    isLoading={stage === 1}
+                  />
+                  <Stage
+                    stageNumber={2}
+                    currentStage={stage}
+                    stageName="Cleaning Text"
+                    isLoading={stage === 2}
+                  />
+                  <Stage
+                    stageNumber={3}
+                    currentStage={stage}
+                    stageName="Extracting Questions"
+                    isLoading={stage === 3}
+                  />
+                  <Stage
+                    stageNumber={4}
+                    currentStage={stage}
+                    stageName="Generating Help"
+                    isLoading={isLoading && stage === 4}
+                  />
+                </div>
+              </>
             )}
-            <div className="w-full h-2 ">
-              {outputLoading && (
-                <>
-                  <p className="text-xl text-black mb-4">
-                    Note: It can take up to 20/30 seconds per question to
-                    generate an analysis.
-                  </p>
-                  <div className="flex space-x-4 mb-4">
-                    <Stage
-                      stageNumber={1}
-                      currentStage={stage}
-                      stageName="Scanning Text"
-                      isLoading={stage === 1}
-                    />
-                    <Stage
-                      stageNumber={2}
-                      currentStage={stage}
-                      stageName="Cleaning Text"
-                      isLoading={stage === 2}
-                    />
-                    <Stage
-                      stageNumber={3}
-                      currentStage={stage}
-                      stageName="Extracting Questions"
-                      isLoading={stage === 3}
-                    />
-                    <Stage
-                      stageNumber={4}
-                      currentStage={stage}
-                      stageName="Generating Help"
-                      isLoading={isLoading && stage === 4}
-                    />
-                  </div>
-                </>
-              )}
-            </div>
-          {/* </div>
-        </div> */}
+          </div>
+        </div>
 
         <div className="mt-8 ">
           {generated && (
