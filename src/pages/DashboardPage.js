e8d23ddaa8fa5510384801d122bd8e7d8436499a
RomanczuG <romanczug@icloud.com>
2023-06-08 13:54:54 +0200

    new

--- src/pages/DashboardPage.js
+++ src/pages/DashboardPage.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { supabase } from "../supabaseClient";
-import { Button, hashFilename, Modal } from "../utils/ToolUtils";
-import axios from "axios";
+import { Button } from "../utils/ToolUtils";
+import { Modal, handleNewClass, handleFileUploadDashboard } from "../utils/DashboardUtils";
 import { ClipLoader } from "react-spinners";
 import { FaPlusCircle, FaFilePdf } from "react-icons/fa";
 export const Dashboard = () => {
@@ -14,10 +14,7 @@ export const Dashboard = () => {
   const [uploadedLoading, setUploadedLoading] = useState(false);
   const [isOpenClass, setIsOpenClass] = useState(false);
   const [isOpenFile, setIsOpenFile] = useState(false);
-  const client = axios.create({
-    // baseURL: "http://127.0.0.1:5000",
-    baseURL: "https://studyboost.uc.r.appspot.com",
-  });
+ 
   const handleFileChange = (e) => {
     setFile(e.target.files[0]);
     setPdfSrc(URL.createObjectURL(e.target.files[0]));
@@ -27,7 +24,7 @@ export const Dashboard = () => {
     fetchClassesWithFiles();
   }, []);
 
-  const fetchClassesWithFiles = async () => {
+  const fetchClassesWithFiles = async (setUserID, setClasses, setSelectedClass) => {
     const { data, errorUser } = await supabase.auth.getSession();
     if (errorUser) {
       console.error("Error getting user session:", errorUser);
@@ -46,76 +43,6 @@ export const Dashboard = () => {
         setSelectedClass(classes[0].id); // set selectedClass to be the id of the first class
       }
     }
-  };
-
-  const handleNewClass = async () => {
-    const { error } = await supabase
-      .from("classes")
-      .insert([{ name: newClass, user_id: userID }]);
-
-    // console.log(supabase.auth.getUser());
-
-    if (error) {
-      console.error("Error creating class:", error);
-    } else {
-      setNewClass("");
-      fetchClassesWithFiles();
-    }
-  };
-  const handleFileUploadDashboard = async () => {
-
-    if (!file) {
-      console.error("No file selected");
-      alert("Please select a file to upload");
-      return;
-    }
-    setUploadedLoading(true);
-    const fileName = file.name;
-    const classId = selectedClass; // selectedClass is already an id
-    const formData = new FormData();
-    const hashedFilename = hashFilename(file.name);
-    const newFile = new File([file], hashedFilename, { type: file.type });
-    formData.append("file", newFile);
-    console.log("Form data:", formData);
-    console.log("Hashed filename" + hashedFilename)
-    console.log("File name" + fileName)
-    console.log("Class id" + classId)
-    const fileSize = file.size;
-    const { error } = await supabase
-      .from("files")
-      .insert([{ file_name: fileName, class_id: classId, hashed_file_name: hashedFilename }]);
-
-    if (error) {
-      console.error("Error inserting record:", error);
-    } else {
-      console.log("File uploaded to supabase");
-      fetchClassesWithFiles();
-    }
-
-    try {
-      const response = await client.post("/upload", formData);
-      console.log("File upload response:", response);
-  
-      if (response.status === 200) {
-        window.sa_event("PDF successfull upload", {
-          filename: file.name,
-          fileSize,
-        });
-        console.log("File uploaded to flask server ");// Store the filename in the state
-        
-      } else {
-        alert("File upload failed");
-        console.error(
-          "File upload failed. Check internet connection and try again."
-        );
-      }
-    } catch (error) {
-      alert("File upload failed. Check internet connection and try again.");
-      window.sa_event("PDF failed upload", { error: error.message });
-      console.error("Error during file upload:", error);
-    }
-
-    setUploadedLoading(false);
   };
 
   return (
@@ -139,7 +66,7 @@ export const Dashboard = () => {
               className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md p-3"
             />
             <button
-              onClick={handleNewClass}
+              onClick={handleNewClass(setNewClass, newClass, userID)}
               className="mt-4 py-2 w-40 text-[#252D62] bg-[#FFC700] hover:bg-[#FF6E00] px-4  border text-md border-[#FFC700] rounded-md transition-all duration-200"
             >
               Create Class
@@ -183,7 +110,7 @@ export const Dashboard = () => {
             {pdfSrc && (
               <>
                 <Button
-                  onClick={handleFileUploadDashboard}
+                  onClick={handleFileUploadDashboard(setUploadedLoading, file, selectedClass)}
                   className="mt-4 py-2 text-[#252D62] bg-[#FFC700] hover:bg-[#FF6E00] px-4  border text-md border-[#FFC700] rounded-md transition-all duration-200"
                 >
                   {uploadedLoading && (
---
+++ src/utils/DashboardUtils.js
@@ -0,0 +1,93 @@
+import { hashFilename } from "./ToolUtils";
+import { supabase } from "../supabaseClient";
+import axios from "axios";
+const client = axios.create({
+    // baseURL: "http://127.0.0.1:5000",
+    baseURL: "https://studyboost.uc.r.appspot.com",
+  });
+export const Modal = ({ children, isOpen, setIsOpen }) => {
+    return (
+      <div 
+        className={`fixed z-50 inset-0 overflow-y-auto bg-gray-500 bg-opacity-75 ${isOpen ? '' : 'hidden'}`} 
+        onClick={() => setIsOpen(false)}
+      >
+        <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
+        onClick={(e) => e.stopPropagation()}
+        >
+          {children}
+        </div>
+      </div>
+    );
+  }
+
+
+ export const handleNewClass = async (setNewClass, newClass, userID) => {
+    const { error } = await supabase
+      .from("classes")
+      .insert([{ name: newClass, user_id: userID }]);
+
+    // console.log(supabase.auth.getUser());
+
+    if (error) {
+      console.error("Error creating class:", error);
+    } else {
+      setNewClass("");
+    //   fetchClassesWithFiles();
+    }
+  };
+
+ export const handleFileUploadDashboard = async (setUploadedLoading, file, selectedClass) => {
+
+    if (!file) {
+      console.error("No file selected");
+      alert("Please select a file to upload");
+      return;
+    }
+    setUploadedLoading(true);
+    const fileName = file.name;
+    const classId = selectedClass; // selectedClass is already an id
+    const formData = new FormData();
+    const hashedFilename = hashFilename(file.name);
+    const newFile = new File([file], hashedFilename, { type: file.type });
+    formData.append("file", newFile);
+    console.log("Form data:", formData);
+    console.log("Hashed filename" + hashedFilename)
+    console.log("File name" + fileName)
+    console.log("Class id" + classId)
+    const fileSize = file.size;
+    const { error } = await supabase
+      .from("files")
+      .insert([{ file_name: fileName, class_id: classId, hashed_file_name: hashedFilename }]);
+
+    if (error) {
+      console.error("Error inserting record:", error);
+    } else {
+      console.log("File uploaded to supabase");
+    //   fetchClassesWithFiles();
+    }
+
+    try {
+      const response = await client.post("/upload", formData);
+      console.log("File upload response:", response);
+  
+      if (response.status === 200) {
+        window.sa_event("PDF successfull upload", {
+          filename: file.name,
+          fileSize,
+        });
+        console.log("File uploaded to flask server ");// Store the filename in the state
+        
+      } else {
+        alert("File upload failed");
+        console.error(
+          "File upload failed. Check internet connection and try again."
+        );
+      }
+    } catch (error) {
+      alert("File upload failed. Check internet connection and try again.");
+      window.sa_event("PDF failed upload", { error: error.message });
+      console.error("Error during file upload:", error);
+    }
+
+    setUploadedLoading(false);
+  };
