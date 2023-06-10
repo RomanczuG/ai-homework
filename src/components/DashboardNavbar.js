0faf0344212ac86c9ac0b69a035931786c62999a
RomanczuG <romanczug@icloud.com>
2023-06-10 16:44:27 +0200

    new

--- src/components/DashboardNavbar.js
+++ src/components/DashboardNavbar.js
@@ -1,38 +1,20 @@
 import React, { useState } from "react";
 import { Link, useNavigate, useLocation } from "react-router-dom";
+import { motion, AnimatePresence } from "framer-motion";
 
 export const DashboardNavbar = ({ onUpgradeClick }) => {
   const navigate = useNavigate();
   const location = useLocation();
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-  const scrollToSection = (sectionId) => {
-    const sectionElement = document.getElementById(sectionId);
-    if (sectionElement) {
-      sectionElement.scrollIntoView({ behavior: "smooth" });
-    }
-  };
-
-  const handleFeaturesClick = () => {
-    if (location.pathname !== "/") {
-      navigate("/");
-    }
-    scrollToSection("features");
-  };
-
-  const handleAuthorClick = () => {
-    if (location.pathname !== "/") {
-      navigate("/");
-    }
-    scrollToSection("about-author");
-  };
-
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
   };
 
-
-
+  const variants = {
+    open: { opacity: 1, height: "auto" },
+    closed: { opacity: 0, height: 0 },
+  };
 
   return (
     <div className="bg-[#F0FFE0] sticky top-0 z-10 px-20 ">
@@ -44,45 +26,47 @@ export const DashboardNavbar = ({ onUpgradeClick }) => {
             </Link>
           </div>
           
-          <div
-            className={`${
-              isMenuOpen ? "block" : "hidden"
-            } md:flex items-center justify-center space-x-6 nav-menu`}
-          >
-            <ul
-              className="flex flex-col md:flex-row items-center space-y-4 md:space-y-0 md:space-x-6"
-            >
-              <li>
-                <Link
-                  to="/dashboard"
-                  className="text-[#252D62] hover:text-[#FF6E00] transition-all duration-200"
-                >
-                  My Classes
-                </Link>
-              </li>
-              <li>
-              <div
-        className="text-[#252D62] hover:text-[#FF6E00] transition-all duration-200"
-        onClick={onUpgradeClick}
-      >
-        Upgrade
-      </div>
-              </li>
-              <li>
-              <Link
-                to="/dashboard/account"
-                className={`text-white bg-[#FFC700] hover:bg-[#FF6E00] px-4 py-2 border text-md border-[#FFC700] rounded-md transition-all duration-200`}
+          <AnimatePresence>
+            {isMenuOpen && (
+              <motion.div
+                className="md:flex items-center justify-center space-x-6 nav-menu"
+                initial="closed"
+                animate="open"
+                exit="closed"
+                variants={variants}
               >
-                Account
-              </Link>
-            </li>
-            </ul>
-            {/* <div className={`md:block`}> */}
-             
-            {/* </div>/ */}
-          </div>
+                <ul className="flex flex-col md:flex-row items-center space-y-4 md:space-y-0 md:space-x-6">
+                  <li>
+                    <Link
+                      to="/dashboard"
+                      className="text-[#252D62] hover:text-[#FF6E00] transition-all duration-200"
+                    >
+                      My Classes
+                    </Link>
+                  </li>
+                  <li>
+                    <div
+                      className="text-[#252D62] hover:text-[#FF6E00] transition-all duration-200 cursor-pointer"
+                      onClick={onUpgradeClick}
+                    >
+                      Upgrade
+                    </div>
+                  </li>
+                  <li>
+                    <Link
+                      to="/dashboard/account"
+                      className={`text-white bg-[#FFC700] hover:bg-[#FF6E00] px-4 py-2 border text-md border-[#FFC700] rounded-md transition-all duration-200`}
+                    >
+                      Account
+                    </Link>
+                  </li>
+                </ul>
+              </motion.div>
+            )}
+          </AnimatePresence>
+
           <div className="md:hidden">
-            <button aria-label="buy product" onClick={toggleMenu}>
+            <button aria-label="Toggle menu" onClick={toggleMenu}>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 20 20"
@@ -99,7 +83,6 @@ export const DashboardNavbar = ({ onUpgradeClick }) => {
             </button>
           </div>
         </div>
-        
       </nav>
     </div>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import { Link, useNavigate, useLocation } from "react-router-dom";
-export const Navbar = () => {  
+import { motion, AnimatePresence } from "framer-motion";
+
+export const Navbar = () => {
   const navigate = useNavigate();
   const location = useLocation();
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -30,72 +32,66 @@ export const Navbar = () => {
     setIsMenuOpen(!isMenuOpen);
   };
 
+  const variants = {
+    open: { opacity: 1, height: "auto" },
+    closed: { opacity: 0, height: 0 },
+  };
 
   return (
     <div className="bg-[#F0FFE0] sticky top-0 z-10 px-20 ">
       <nav className="mx-auto px-6 md:px-20 py-6">
         <div className="flex items-center justify-between md:justify-between">
-          <div className= {`${isMenuOpen ? "hidden" : "block"} text-[#252D62] font-semibold text-lg`}>
+          <div
+            className={`${
+              isMenuOpen ? "hidden" : "block"
+            } text-[#252D62] font-semibold text-lg`}
+          >
             <Link to="/" className="hover:text-[#FF6E00]">
               Study Smarter Now!
             </Link>
           </div>
-          
-          <div
-            className={`${
-              isMenuOpen ? "block" : "hidden"
-            } md:flex items-center justify-center space-x-6 nav-menu`}
-          >
-            <ul
-              className="flex flex-col md:flex-row items-center space-y-4 md:space-y-0 md:space-x-6"
-            >
-              {/* <li>
-                <span
-                  onClick={handleFeaturesClick}
-                  className="text-[#252D62] hover:text-[#FF6E00] cursor-pointer transition-all duration-200"
-                >
-                  Features
-                </span>
-              </li>
-              <li>
-                <span
-                  onClick={handleAuthorClick}
-                  className="text-[#252D62] hover:text-[#FF6E00] cursor-pointer transition-all duration-200"
-                >
-                  About the Author
-                </span>
-              </li> */}
-              <li>
-                <Link
-                  to="/faq"
-                  className="text-[#252D62] hover:text-[#FF6E00] transition-all duration-200"
-                >
-                  FAQ
-                </Link>
-              </li>
-              <li>
-                <Link
-                  to="/how-it-works"
-                  className="text-[#252D62] hover:text-[#FF6E00] transition-all duration-200"
-                >
-                  How It Works
-                </Link>
-              </li>
-              <li>
-              <Link
-                to="/dashboard"
-                className={`text-white bg-[#FFC700] hover:bg-[#FF6E00] px-4 py-2 border text-md border-[#FFC700] rounded-md transition-all duration-200`}
+
+          <AnimatePresence>
+            {isMenuOpen && (
+              <motion.div
+                className="md:flex items-center justify-center space-x-6 nav-menu"
+                initial="closed"
+                animate="open"
+                exit="closed"
+                variants={variants}
               >
-                Get started &rarr;
-              </Link>
-            </li>
-            </ul>
-            {/* <div className={`md:block`}> */}
-             
-            {/* </div>/ */}
-          </div>
+                <ul className="flex flex-col md:flex-row items-center space-y-4 md:space-y-0 md:space-x-6">
+                  <li>
+                    <Link
+                      to="/faq"
+                      className="text-[#252D62] hover:text-[#FF6E00] transition-all duration-200"
+                    >
+                      FAQ
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      to="/how-it-works"
+                      className="text-[#252D62] hover:text-[#FF6E00] transition-all duration-200"
+                    >
+                      How It Works
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      to="/dashboard"
+                      className={`text-white bg-[#FFC700] hover:bg-[#FF6E00] px-4 py-2 border text-md border-[#FFC700] rounded-md transition-all duration-200`}
+                    >
+                      Get started &rarr;
+                    </Link>
+                  </li>
+                </ul>
+              </motion.div>
+            )}
+          </AnimatePresence>
+
           <div className="md:hidden">
-            <button aria-label="buy product" onClick={toggleMenu}>
+            <button aria-label="Toggle menu" onClick={toggleMenu}>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 20 20"
@@ -112,7 +108,6 @@ export const Navbar = () => {
             </button>
           </div>
         </div>
-        
       </nav>
     </div>
   );
