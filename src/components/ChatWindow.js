4b2b65c42d256abf03bc7914930d55f6a9ed1a4e
RomanczuG <romanczug@icloud.com>
2023-06-12 15:27:10 +0200

    new

--- src/components/ChatWindow.js
+++ src/components/ChatWindow.js
@@ -1,57 +1,63 @@
-import React, { useState } from 'react';
-import { Button } from '../utils/ToolUtils';
+import React, { useState } from "react";
+import { Button } from "../utils/ToolUtils";
+import axios from "axios";
 
-const ChatWindow = ({hashedFaissFilename} ) => {
-    const [messages, setMessages] = useState([]);
-    const [newMessage, setNewMessage] = useState('');
-  
-    // Mock send message function
-    const sendMessage = (e) => {
-      e.preventDefault();
-      if (newMessage !== '') {
-        setMessages([...messages, { text: newMessage, sender: 'you' }]);
-        fetch('/chat', {   // replace '/chat' with the URL of your chat endpoint
-          method: 'POST',
-          headers: { 'Content-Type': 'application/json' },
-          body: JSON.stringify({ question: newMessage, hashedFaissFilename: hashedFaissFilename }),
+const ChatWindow = ({ hashedFaissFilename }) => {
+  const client = axios.create({
+    // baseURL: "http://127.0.0.1:5000",
+    baseURL: "https://studyboost.uc.r.appspot.com",
+  });
+  const [messages, setMessages] = useState([]);
+  const [newMessage, setNewMessage] = useState("");
+
+  // Mock send message function
+  const sendMessage = (e) => {
+    e.preventDefault();
+    if (newMessage !== '') {
+      setMessages([...messages, { text: newMessage, sender: 'you' }]);
+      client.post('/chat', { question: newMessage, hashedFaissFilename })
+        .then(response => {
+          const data = response.data;
+          setMessages(prevMessages => [...prevMessages, { text: data.message, sender: 'bot' }]);
         })
+        .catch(e => console.error('Error:', e));
+      setNewMessage('');
+    }
+  };
 
-          .then(response => response.json())
-          .then(data => setMessages([...messages, { text: data.message, sender: 'bot' }]))
-          .catch(e => console.error('Error:', e));
-        setNewMessage('');
-      }
-    };
-  
-    return (
-      <div className="flex flex-col h-[75vh]">
-        {/* Messages */}
+  return (
+    <div className="flex flex-col h-[75vh]">
+      {/* Messages */}
 
-        <div className="overflow-auto mb-4 flex-grow">
-          {messages.map((message, i) => (
-            <div 
-              key={i} 
-              className={`
+      <div className="overflow-auto mb-4 flex-grow">
+        {messages.map((message, i) => (
+          <div
+            key={i}
+            className={`
                 my-2 px-4 py-2 rounded-lg 
-                ${message.sender === 'you' ? 'bg-orange-300 ml-auto' : 'bg-orange-500 mr-auto'}
+                ${
+                  message.sender === "you"
+                    ? "bg-orange-300 ml-auto"
+                    : "bg-orange-500 mr-auto"
+                }
               `}
-            >
-              <b>{message.sender}:</b> {message.text}
-            </div>
-          ))}
-        </div>
-  
-        {/* Send message form */}
-        <form onSubmit={sendMessage} className="mt-auto flex">
-          <input
-            value={newMessage}
-            onChange={(e) => setNewMessage(e.target.value)}
-            className="border rounded px-2 py-1 mr-2 flex-grow"
-          />
-          <Button onClick={sendMessage}>Send</Button>
-        </form>
+          >
+            <b>{message.sender}:</b> {message.text}
+          </div>
+        ))}
       </div>
-    );
+
+      {/* Send message form */}
+      <form onSubmit={sendMessage} className="mt-auto flex">
+        <input
+          value={newMessage}
+          onChange={(e) => setNewMessage(e.target.value)}
+          className="border rounded px-2 py-1 mr-2 flex-grow"
+        />
+        <Button onClick={sendMessage}>Send</Button>
+      </form>
+    </div>
+  );
 };
 
 export default ChatWindow;
