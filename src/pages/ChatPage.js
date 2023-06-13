3650cb39693ebda24eef0fc138f6d67166a0151d
RomanczuG <romanczug@icloud.com>
2023-06-13 17:00:24 +0200

    new

--- src/pages/ChatPage.js
+++ src/pages/ChatPage.js
@@ -1,4 +1,3 @@
-
 import ChatWindow from "../components/ChatWindow";
 import { motion } from "framer-motion";
 import { useLocation } from "react-router-dom";
@@ -17,31 +16,28 @@ export const ChatPage = () => {
   const hashedFilename = location.state.hashedFilename;
   const [numPages, setNumPages] = useState(null);
   const [pdfUrl, setPdfUrl] = useState(null);
-  console.log('In ChatPage, hashedFaissFilename:', hashedFaissFilename);
-  console.log('In ChatPage, pdfFilename:', pdfFilename);
-  console.log('In ChatPage, hashedFilename:', hashedFilename);
-  
-
+  console.log("In ChatPage, hashedFaissFilename:", hashedFaissFilename);
+  console.log("In ChatPage, pdfFilename:", pdfFilename);
+  console.log("In ChatPage, hashedFilename:", hashedFilename);
 
   useEffect(() => {
-    client.get(`/download_pdf/${hashedFilename}`, {
-      responseType: 'blob' // Force to receive data in a Blob Format
-    })
-    .then(response => {
-      console.log('In ChatPage, response:', response);
-      //Create a Blob from the PDF Stream
-      const file = new Blob(
-        [response.data], 
-        {type: 'application/pdf'});
-      //Build a URL from the file
-      const fileURL = URL.createObjectURL(file);
-      console.log('In ChatPage, fileURL:', fileURL);
-      //Open the URL on new Window
-      setPdfUrl(fileURL);
-    })
-    .catch(error => {
-      console.log(error);
-    });
+    client
+      .get(`/download_pdf/${hashedFilename}`, {
+        responseType: "blob", // Force to receive data in a Blob Format
+      })
+      .then((response) => {
+        console.log("In ChatPage, response:", response);
+        //Create a Blob from the PDF Stream
+        const file = new Blob([response.data], { type: "application/pdf" });
+        //Build a URL from the file
+        const fileURL = URL.createObjectURL(file);
+        console.log("In ChatPage, fileURL:", fileURL);
+        //Open the URL on new Window
+        setPdfUrl(fileURL);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }, [hashedFilename]);
 
   return (
@@ -50,16 +46,37 @@ export const ChatPage = () => {
       animate={{ opacity: 1, y: 0 }}
       transition={{ duration: 0.5 }}
       // style={{ height: "calc(100vh - (60px + 75px))" }}
+      className="h-full"
     >
-      <div className="flex flex-row h-full">
+      <div className="flex flex-col sm:flex-row h-full">
         {/* Sidebar */}
-        <div className="w-1/2 bg-gray-200 p-4">
-        <h2 className="mb-4 font-bold">{pdfFilename}</h2>
-        {pdfUrl && <iframe src={pdfUrl} width="100%" height="600px" title="PDF Document"></iframe>}
-      </div>
+        <div className="w-full sm:w-1/2 bg-gray-200 p-4">
+          <h2 className="mb-4 font-bold">{pdfFilename}</h2>
+          {pdfUrl && (
+            <div className="w-full h-full overflow-auto rounded-lg shadow-lg">
+              <iframe
+                src={pdfUrl}
+                title="PDF Document"
+                style={{
+                  border: "none",
+                  width: "100%",
+                  height: "100%",
+                }}
+              />
+            </div>
+          )}
+          {pdfUrl && (
+            <button
+              className="mt-4 px-4 py-2 rounded bg-blue-500 text-white"
+              onClick={() => window.open(pdfUrl, "_blank")}
+            >
+              Open PDF in new tab
+            </button>
+          )}
+        </div>
 
         {/* Chat area */}
-        <div className="w-1/2 p-4 overflow-auto">
+        <div className="w-full sm:w-1/2 p-4 overflow-auto mt-4 sm:mt-0">
           {/* <h2 className="mb-4 font-bold">Chat with {selectedChat.name}</h2> */}
           <h2 className="mb-4 font-bold">Chat </h2>
           {/* The chat messages will be displayed here */}
