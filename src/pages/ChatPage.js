e319fee72ee9e47e2cb5dfe6154c5f65c84a5fe8
RomanczuG <romanczug@icloud.com>
2023-06-13 17:06:34 +0200

    new

--- src/pages/ChatPage.js
+++ src/pages/ChatPage.js
@@ -46,15 +46,15 @@ export const ChatPage = () => {
       initial={{ opacity: 0, y: 50 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{ duration: 0.5 }}
-      style={{ height: "calc(80vh)" }}
+      style={{ height: "calc(100vh - 80px)" }}  // 80px is approximated navbar height
       className="h-full"
     >
       <div className="flex flex-col sm:flex-row h-full">
         {/* Sidebar */}
-        <div className="w-full sm:w-1/2 bg-gray-200 p-4">
+        <div className="w-full sm:w-1/2 bg-gray-200 p-4 flex flex-col">
           <h2 className="mb-4 font-bold">{pdfFilename}</h2>
           {pdfUrl && (
-            <div className="w-full h-full overflow-auto rounded-lg shadow-lg">
+            <div className="w-full flex-grow overflow-auto rounded-lg shadow-lg">
               <iframe
                 src={pdfUrl}
                 title="PDF Document"
@@ -67,27 +67,16 @@ export const ChatPage = () => {
             </div>
           )}
           {pdfUrl && (
-            <Button
-            onClick={() => window.open(pdfUrl, "_blank")}
-            >
-
-
-Open PDF in new tab
+            <Button onClick={() => window.open(pdfUrl, "_blank")} className="mt-4">
+              Open PDF in new tab
             </Button>
-            // <button
-            //   className="mt-4 px-4 py-2 rounded bg-blue-500 text-white"
-             
-            // >
-              
-            // </button>
           )}
         </div>
 
         {/* Chat area */}
         <div className="w-full sm:w-1/2 p-4 overflow-auto mt-4 sm:mt-0">
-          {/* <h2 className="mb-4 font-bold">Chat with {selectedChat.name}</h2> */}
           <h2 className="mb-4 font-bold">Chat </h2>
-          {/* The chat messages will be displayed here */}
+
           <ChatWindow hashedFaissFilename={hashedFaissFilename} />
         </div>
       </div>
