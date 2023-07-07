89abaa4eb974b8653c447479d5957dabef23adf8
RomanczuG <romanczug@icloud.com>
2023-07-07 14:09:11 -0400

    new

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,15 +4,58 @@ import { Link } from 'react-router-dom';
 export const Footer = () => {
   return (
     <footer className="bg-[#F0FFE0] py-4 ">
-      <div className="container mx-auto px-6 md:px-4 flex justify-center items-center">
+      <div className="container mx-auto px-6 md:px-4 flex flex-col justify-center items-center">
         <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-4">
-          <Link to="/privacy-policy" className="text-black font-semibold text-base hover:text-[#FF6E00] text-center">
+          {/* Lets add information about Powered by OpenAI, Vercel, Supabase, Tailwind CSS, Flask, Google Cloud */}
+          <p className="text-black font-semibold text-base text-center">
+            Powered by{' '}
+            <a href="https://openai.com/" target="_blank" rel="noreferrer" className="hover:text-[#FF6E00]">
+              OpenAI
+            </a>
+            ,{' '}
+            <a href="https://vercel.com/" target="_blank" rel="noreferrer" className="hover:text-[#FF6E00]">
+              Vercel
+            </a>
+            ,{' '}
+            <a href="https://supabase.io/" target="_blank" rel="noreferrer" className="hover:text-[#FF6E00]">
+              Supabase
+            </a>
+            ,{' '}  
+            <a href="https://tailwindcss.com/" target="_blank" rel="noreferrer" className="hover:text-[#FF6E00]">
+              Tailwind CSS
+            </a>
+            ,{' '}
+            <a href="https://flask.palletsprojects.com/en/2.0.x/" target="_blank" rel="noreferrer" className="hover:text-[#FF6E00]">
+              Flask
+            </a>
+            ,{' '}
+            <a href="https://cloud.google.com/" target="_blank" rel="noreferrer" className="hover:text-[#FF6E00]">
+              Google Cloud
+            </a>
+          </p>
+         
+        </div>
+        {/* Let now put stuff below */}
+        <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-4">
+        <Link to="/privacy-policy" className="text-black font-semibold text-base hover:text-[#FF6E00] text-center">
             Privacy Policy
           </Link>
           <Link to="/terms-of-service" className="text-black font-semibold text-base hover:text-[#FF6E00] text-center">
             Terms of Service
           </Link>
-        </div>
+          </div>
+
+          {/* Copyright to 732 Development */}
+          <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-4">
+          <p className="text-black font-semibold text-base text-center">
+            © 2023 732 Development
+          </p>
+
+          </div>
+
+
+
+          
       </div>
     </footer>
   );
